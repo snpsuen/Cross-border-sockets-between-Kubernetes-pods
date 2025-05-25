691ed9e279e57fb7cb0aa409ebe9d180b73f856c
Sunny Paul Suen <66404197+snpsuen@users.noreply.github.com>
2025-05-25 15:51:16 +0100

    Update simkubexec.c

--- source/simkubexec.c
+++ source/simkubexec.c
@@ -15,7 +15,6 @@
 #include <time.h>
 #include <sys/wait.h>
 
-#define NSLEN 8
 #define MINLEN 64
 #define MEDLEN 128
 #define MAXLEN 256
@@ -60,35 +59,30 @@ int set_con_ns(char* container) {
 int main(int argc, char* argv[]) {
         int i;
         pid_t child;
-        char* token;
-        char container[MINLEN], execstring[MAXLEN];
-        char* execargv[MAXLEN];
+        char* container;
+        char** execargv;
 
-        memset(container, 0, sizeof(container));
-        printf("Enter the container name: ");
-        fgets(container, sizeof(container), stdin);
-        container[strlen(container) - 1] = 0;
+        if (argc < 2)
+                container = "kube-proxy";
+        else
+                container = argv[1];
 
+        if (argc < 3) {
+                execargv = calloc(2, siseof(char*));
+                excargv = {"sh", "NULL"};
+        }
+        else {
+                execargv = calloc(argc - 1, siseof(char*));
+                for (i = 0; i < argc - 2; i++)
+                        execargv[i] = argv[i + 2];
+                execargv[argc - 2] = NULL;
+        }
+                        
         set_con_ns(container);
         if ((child = fork()) < 0)
                 perror("fork error");
 
         if (child == 0) {
-                memset(execstring, 0, sizeof(execstring));
-                printf("Enter the command to exec in %s: ", container);
-                fgets(execstring, sizeof(execstring), stdin);
-                execstring[strlen(execstring) - 1] = 0;
-
-                i = 0;
-                token = strtok(execstring, " ");
-                while (token != NULL) {
-                        execargv[i] = token;
-                        /* printf("execargv[%d] = %s\n", i, execargv[i]); */
-                        token = strtok(NULL, " ");
-                        i++;
-                }
-                execargv[i] = NULL;
-
                 if (execvp(execargv[0], execargv) < 0) {
                         perror("execvp error");
                         exit(1);
